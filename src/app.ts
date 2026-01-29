b91090bd8d660571a98b6173e1b2d0c5364cd8eb
bagus25dzikri06 <bagusdzikri@gmail.com>
2026-01-29 13:46:33 +0700

    Update in app.ts and config for TS for deploying in Vercel

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import authRoutes from './routes/auth.route'
 import { authenticate } from './middlewares/auth.middleware'
@@ -9,10 +9,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
-app.get('/', (req, res) => {
+app.get('/', (req : Request, res : Response) => {
     res.send('SportOn Backend Is Running')
 })
-app.get('/middleware', authenticate, (req, res) => {
+app.get('/middleware', authenticate, (req : Request, res : Response) => {
     res.send('This endpoint needs an authentication')
 })
 
